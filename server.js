bb64d674df719b19be9fb719f32008b4fa532881
Omar Riaz <omar@Omars-MBP.attlocal.net>
2021-04-22 08:10:56 -0500

    added routes and test skeletons

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.listen(port, () => {
  console.log("Movie Night API started on port " + port);
 });
 
+const bodyParser = require('body-parser');
+app.use(bodyParser);
+
+const MovieModel = mongoose.model('Movie', mySchema);
+const MovieNightModel = mongoose.model('MovieNight', mySchema);
+
+
 // Info of the movie night
 app.get('/info')
 
@@ -17,14 +24,42 @@ app.route('/movienights')
 // Make a call to the imdb api
 app.get('/movieinfo/:movieName')
 
-app.post('/create')
-app.post('/join')
-app.post('/delete')
-app.post('/start')
-app.post('/nominate')
-app.post('/vote')
-app.post('/settings')
-app.post('/invite')
-app.post('/kick')
-app.post('/ban')
-app.post('/mod')
+// app.post('/create')
+// app.post('/join')
+// app.post('/delete')
+// app.post('/start')
+// app.post('/nominate')
+// app.post('/vote')
+// app.post('/settings')
+// app.post('/invite')
+// app.post('/kick')
+// app.post('/ban')
+// app.post('/mod')
+
+movieRouter = express.Router()
+movieNightRouter = express.Router()
+
+// at this layer, we can add our logic to verify valid api call
+// ex. uses a session token, or has a certain payload property
+movieRouter.use(function moviePrep (req, res, next) {
+  next()
+});
+
+movieNightRouter.use(function movieNightPrep (req, res, next) {
+  next()
+});
+
+
+movieRouter.get('/movie/:id', (req, res) =>{
+  MovieModel.findOne(req.params.id)
+});
+movieRouter.post('/movie', (req, res) =>{
+  MovieModel.save(req.body)
+});
+
+movieNightRouter.get('/movie-night/:id', (req, res) =>{
+  MovieNightModel.findOne(req.params.id)
+});
+movieNightRouter.post('/movie-night', (req, res) =>{
+  MovieNightModel.save(req.body.)
+});
---
+++ server.test.js
@@ -0,0 +1,15 @@
+test('test add movie', () => {
+
+});
+
+test('test get a movie', () => {
+
+});
+
+test('test add movie night', () => {
+
+});
+
+test('test get a movie night', () => {
+
+});
