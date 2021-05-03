0b3f2c795041e1e26f2cefe6fdd9a9d5cf2aa55b
Josh <joshuaj1397@gmail.com>
2021-05-02 23:00:36 -0500

    Added OMDBAPI results to movie models

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 
 const express = require("express"),
-    app = express(),
-    port = process.env.PORT || 3005;
+  app = express(),
+  port = process.env.PORT || 3005;
 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const MovieNightModel = require('./api/models/user');
 const MovieModel = require('./api/models/movie');
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 // app.listen(port, () => {
 //  console.log("Movie Night API started on port " + port);
 // });
@@ -49,55 +50,67 @@ movieNightRouter = express.Router();
 //
 // // at this layer, we can add our logic to verify valid api call
 // // ex. uses a session token, or has a certain payload property
-movieRouter.use(function moviePrep (req, res, next) {
+movieRouter.use(function moviePrep(req, res, next) {
   next();
 });
 
-movieNightRouter.use(function movieNightPrep (req, res, next) {
+movieNightRouter.use(function movieNightPrep(req, res, next) {
   next();
 });
 
 
-app.get('/api/movies/:id', (req, res) =>{
-  try{
-    const query = MovieModel.findOne({_id: req.params.id});
+app.get('/api/movies/:id', (req, res) => {
+  try {
+    const query = MovieModel.findOne({ _id: req.params.id });
     query.then(doc => res.send(doc));
     // return res.send(movie);
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(400).send('movie not found');
   }
 });
 
-app.post('/api/movies', (req, res) =>{
-  try{
+app.post('/api/movies', (req, res) => {
+  try {
     // Before adding the movie, search it in omdb and get some stats for seeding
     const omdbResults = await fetch('http://www.omdbapi.com/?apikey=' + process.env.OMDB_API_KEY + '&t=' + req.body.name);
     omdbResults = await omdbResults.json();
-    // TODO: Save ratings to movie model
+    // Map ratings
+    const ratings = omdbResults.Ratings.reduce((ratingMap, rating) => {
+      ratingMap[rating.Source] = rating.value;
+      return ratingMap;
+    });
+    const movie = {
+      name: omdbResults.Title,
+      year: omdbResults.Year,
+      metacritic_rating: ratings['Metacritic'],
+      rotten_tomatoes_rating: ratings['Rotten Tomatoes'],
+      imdb_rating: ratings['Internet Movie Database'],
+      votes: null
+    }
     // add new movie to movies
-    const query = MovieModel.findOneAndUpdate({_id: req.body.id}, req.body, {new: true, upsert: true});
+    const query = MovieModel.findOneAndUpdate({ _id: req.body.id }, movie, { new: true, upsert: true });
     query.then(doc => res.status(201).send(doc));
     // add the movie to a movie night
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(400).send('movie not created');
   }
 });
 
 
-app.get('/api/movieNights/:id', (req, res) =>{
+app.get('/api/movieNights/:id', (req, res) => {
   // find a movie night
   movieNight = MovieNightModel.findOne(req.params.id);
-  return res.send({'movieNight': movieNight});
+  return res.send({ 'movieNight': movieNight });
 });
 
-app.post('/api/movieNights', (req, res) =>{
+app.post('/api/movieNights', (req, res) => {
   //create a movie night
   MovieNightModel.save(req.body.movieNight);
-  return res.send({'movieNight': req.body.movieNight});
+  return res.send({ 'movieNight': req.body.movieNight });
 });
 
 module.exports = app;
