9fd62db8922f2bdb3a278460499516731f3c15c9
Josh <joshuaj1397@gmail.com>
2021-05-05 20:27:57 -0500

    Normalized data while seeding

--- api/controllers/movieNightController.js
+++ api/controllers/movieNightController.js
@@ -12,5 +12,10 @@ const seed = (movies) => {
 }
 
 const getAvgRating = (movie) => {
-    return (movie.metacritic_rating + movie.rotten_tomatoes_rating + movie.imdb_rating * 10) / 3;
+    let metacriticRatingSplit = movie.metacritic_rating.split('/');
+    let imdbRatingSplit = movie.imdb_rating.split('/');
+    let rottenTomatoesRatingSplit = movie.rotten_tomatoes_rating.substring(0, movie.rotten_tomatoes_rating.length - 1);
+    let metacriticRating = (parseInt(metacriticRatingSplit[0], 10) / parseInt(metacriticRatingSplit[1], 10)) * 10;
+    let rottenTomatoesRating = parseInt(rottenTomatoesRatingSplit) / 10;
+    return (metacriticRating + rottenTomatoesRating + parseFloat(imdbRatingSplit[0])) / 3;
 }
